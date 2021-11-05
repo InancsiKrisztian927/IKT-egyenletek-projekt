8aa88dde1530550d13f7d337fc3aced1d504fd49
InancsiKrisztian927 <35177338+InancsiKrisztian927@users.noreply.github.com>
2021-11-05 07:54:57 +0100

    Add files via upload

---
+++ egyenletek_vegleges.py
@@ -0,0 +1,51 @@
+import math
+
+def menu():
+    print("[1]Elsőfokú egyenlet")
+    print("[2]Másodfokú egyenlet")
+    print("[0]Kilépés")
+
+menu()
+option = int(input("Válassz a lehetőségek közül: "))
+
+while(option != -1):
+    if (option == 1):
+        print("\tElsőfokú egyenlet: a*x+b=c")
+
+        a = int(input("\tKérem az a együtthatót: "))
+        b = int(input("\tKérem a b együtthatót: "))
+        c = int(input("\tKérem a c együtthatót: "))
+        
+        x1 = (c-b)/a
+
+
+        txt= "\t\tMegoldás: "+ str(x1)
+        print(txt)
+
+    elif (option == 2):
+        print("\tMásodfokú egyenlet" )
+
+        a = int(input("\tKérem az a együtthatót: "))
+        b = int(input("\tKérem a b együtthatót: "))
+        c = int(input("\tKérem a c együtthatót: "))
+
+        s = b*b - 4*a*c
+        if (s < 0):
+            txt = "\t\tNincs megoldás."
+            print(txt)
+        else:   
+            x1 = (-1*b  + math.sqrt(s))/2 * a
+            x2 = (-1*b  - math.sqrt(s))/2 * a
+
+            txt = "\t\tMegoldás: " + str(x1)+ "\t"+str(x2)
+            print(txt)
+            
+    elif (option == 0):
+        quit()
+        
+    else:
+        print("Érvénytelen lehetőség.")
+
+    print()
+    menu()
+    option = int(input("Válassz a lehetőségek közül: "))
